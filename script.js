668a4ea0781534835f23afc3ba2bf0685e0b21ca
Guicoding01 <guilhermefloyd2017@gmail.com>
2024-01-22 12:43:47 -0300

    paralax

---
+++ script.js
@@ -0,0 +1,61 @@
+const imageCarousel = document.getElementById('image-carousel'); /* div que esta as img dentro*/
+const imgCarousel = document.getElementById('img-carousel');
+const images = document.querySelectorAll('.carousel-image'); /* div que armazena as img */
+const prevButton = document.querySelector('.carousel-button:nth-child(1)'); /* pegou os botoes pelas posiçoes 1 e 2 */
+const nextButton = document.querySelector('.carousel-button:nth-child(2)'); /* pegou os botoes pelas posiçoes 1 e 2 */
+const prevbtn = document.querySelector('.carousel-btn:nth-child(1)'); /* pegou os botoes pelas posiçoes 1 e 2 */
+const nextbtn = document.querySelector('.carousel-btn:nth-child(2)'); /* pegou os botoes pelas posiçoes 1 e 2 */
+
+/* index para começar do 0 */
+let currentIndex = 0;
+
+/* açao do container demonstrativo de treino */ 
+prevButton.addEventListener('click', prevImage); 
+nextButton.addEventListener('click', nextImage);
+
+/* açao do container metodologias*/
+prevbtn.addEventListener('click', prevImg); 
+nextbtn.addEventListener('click', nextImg);
+
+
+/* funçoes do container demonstrativo de treino */ 
+
+function prevImage() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateCarousel();
+}
+
+function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateCarousel();
+}
+
+function updateCarousel() {
+    const newTransformValue = - currentIndex * 32 + '%';
+    imageCarousel.style.transform = 'translateX(' + newTransformValue + ')';
+}
+
+/* açao do container metodologias*/
+
+function prevImg() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateCarousel_2();
+}
+
+function nextImg() {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateCarousel_2();
+}
+
+function updateCarousel_2() {
+    const newTransformValue = - currentIndex * 32 + '%';
+    imgCarousel.style.transform = 'translateX(' + newTransformValue + ')';
+}
+
+// efeito paralax
+
+window.sr = ScrollReveal({reset : true});
+
+sr.reveal('h2' ,{duration : 1000});
+sr.reveal('#img-carousel' ,{duration : 3000});
+sr.reveal('#image-carousel' ,{duration : 3000});
